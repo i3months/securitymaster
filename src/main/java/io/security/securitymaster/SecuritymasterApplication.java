ff1d23e4004fc9c22e0717c361684a1666335394
i3months <waysoleward01@naver.com>
2024-05-26 19:24:21 +0900

    first commit

---
+++ src/main/java/io/security/securitymaster/SecuritymasterApplication.java
@@ -0,0 +1,13 @@
+package io.security.securitymaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritymasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritymasterApplication.class, args);
+	}
+
+}
