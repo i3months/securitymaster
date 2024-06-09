f2578db519e91aa207042e555969367fc1e8d036
i3months <waysoleward01@naver.com>
2024-06-09 13:46:52 +0900

    Authorize

--- src/main/java/io/security/securitymaster/SecurityConfig.java
+++ src/main/java/io/security/securitymaster/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.security.securitymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.Customizer;
@@ -19,6 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.http.HttpServletRequest;
@@ -34,6 +36,14 @@ public class SecurityConfig {
         
         AuthenticationManagerBuilder builder = http.getSharedObject(AuthenticationManagerBuilder.class);
         AuthenticationManager authenticationManager = builder.build();
+
+        http.authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/user").hasAuthority("USER")
+            .requestMatchers("/mypage/**").hasAuthority("USER")
+            .requestMatchers(HttpMethod.GET, "/**").hasAuthority("read")
+            .requestMatchers(RegexRequestMatcher.regexMatcher("/resource/[A-Z]")).hasAuthority("USER")
+            .anyRequest().authenticated()
+        );
 
         http.sessionManagement(session -> session
             .sessionFixation(sessionFixation -> sessionFixation.changeSessionId())
