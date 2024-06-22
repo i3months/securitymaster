317040f57d8b308785bb72e4aea99aa5b4d5ca2f
i3months <waysoleward01@naver.com>
2024-06-22 20:11:39 +0900

    JSR-250

--- src/main/java/io/security/securitymaster/MethodController.java
+++ src/main/java/io/security/securitymaster/MethodController.java
@@ -1,5 +1,6 @@
 package io.security.securitymaster;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -7,28 +8,41 @@ import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.security.RolesAllowed;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
 public class MethodController {
     
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @Secured("ROLE_USER")
     public String test() {
         return "test";
     }
 
-    @PostAuthorize("hasAuthority('ROLE_ADMIN') and returnObject.isSecure")
-    public Account secureAccount(String name, String secure) {
-        return new Account(name, "Y".equals(secure));
+    @GetMapping("path")
+    @RolesAllowed("ADMIN")
+    public String getMethodName(@RequestParam String param) {
+        return new String();
     }
+    
 
-    @PostMapping("/writelist")
-    public List<Account> writeList(@RequestBody List<Account> data) {
+    // @PostAuthorize("hasAuthority('ROLE_ADMIN') and returnObject.isSecure")
+    // public Account secureAccount(String name, String secure) {
+    //     return new Account(name, "Y".equals(secure));
+    // }
+
+    // @PostMapping("/writelist")
+    // public List<Account> writeList(@RequestBody List<Account> data) {
         
-        return dataService.();
+    //     return dataService.();
         
-    }
+    // }
     
 }
